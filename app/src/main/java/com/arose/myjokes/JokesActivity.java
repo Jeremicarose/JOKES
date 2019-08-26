deffae283608d32aabf1427f9d9534f7ca082732
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-26 05:59:21 +0300

    import android arrayadapter

--- app/src/main/java/com/arose/myjokes/JokesActivity.java
+++ app/src/main/java/com/arose/myjokes/JokesActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 public class JokesActivity extends AppCompatActivity {
 
