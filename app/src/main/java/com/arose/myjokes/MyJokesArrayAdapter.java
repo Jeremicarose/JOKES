0adcb776fb084bfba39c1f06e6d0f1a80e821a87
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-26 21:21:56 +0300

    import content.context

--- app/src/main/java/com/arose/myjokes/MyJokesArrayAdapter.java
+++ app/src/main/java/com/arose/myjokes/MyJokesArrayAdapter.java
@@ -1,4 +1,32 @@
 package com.arose.myjokes;
 
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyJokesArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+
+
+    public MyJokesArrayAdapter(Context mContext, int resource, String[] mRestaurants, String[] mCuisines) {
+        super(mContext, resource);
+        this.mContext = mContext;
+
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String restaurant = mRestaurants[position];
+
+        return String.format("%s \nServes great: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public  int getCount() {
+        return mRestaurants.length;
+    }
+}
+
+
 public class MyJokesArrayAdapter {
 }
