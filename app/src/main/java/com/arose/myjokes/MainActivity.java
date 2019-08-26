b18797969672fde612e4832683d6328e4a1efe47
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-26 05:52:44 +0300

    add editText

--- app/src/main/java/com/arose/myjokes/MainActivity.java
+++ app/src/main/java/com/arose/myjokes/MainActivity.java
@@ -2,13 +2,28 @@ package com.arose.myjokes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.findJokesButton) Button mFindJokesButton;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFindJokesButton.setOnClickListener(this);
+
+
     }
 }
