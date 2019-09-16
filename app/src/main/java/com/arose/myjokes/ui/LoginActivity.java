f53d24e60aad7c5d411a5a248715ae9dd945448b
Jeremic Arose <arosejeremic5@gmail.com>
2019-09-16 09:48:13 +0300

    add loging auth

--- app/src/main/java/com/arose/myjokes/ui/LoginActivity.java
+++ app/src/main/java/com/arose/myjokes/ui/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.arose.myjokes.ui;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,48 +17,49 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     public static final String TAG = LoginActivity.class.getSimpleName();
-
     @BindView(R.id.passwordLoginButton) Button mPasswordLoginButton;
     @BindView(R.id.emailEditText) EditText mEmailEditText;
     @BindView(R.id.passwordEditText) EditText mPasswordEditText;
     @BindView(R.id.registerTextView) TextView mRegisterTextView;
-
     private FirebaseAuth mAuth;
-
-
     private FirebaseAuth.AuthStateListener mAuthListener;
     private ProgressDialog mAuthProgressDialog;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         ButterKnife.bind(this);
-
-        mAuth = FirebaseAuth.getInstance( );
+        mAuth = FirebaseAuth.getInstance();
+        createAuthProgressDialog();
+        mRegisterTextView.setOnClickListener(this);
         mPasswordLoginButton.setOnClickListener(this);
-
-        mAuthListener = new FirebaseAuth.AuthStateListener( ) {
-
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser( );
+                FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
-                    finish( );
-                    createAuthProgressDialog();
+                    finish();
                 }
             }
         };
+    }
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if (view == mPasswordLoginButton) {
+            loginWithPassword();
+        }
     }
     private void createAuthProgressDialog() {
         mAuthProgressDialog = new ProgressDialog(this);
@@ -71,10 +70,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void loginWithPassword() {
         String email = mEmailEditText.getText().toString().trim();
         String password = mPasswordEditText.getText().toString().trim();
-        if (email.equals("")) {
-            mEmailEditText.setError("Please enter your email");
-            return;
-        }
         if (password.equals("")) {
             mPasswordEditText.setError("Password cannot be blank");
             return;
@@ -82,7 +77,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mAuthProgressDialog.show();
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         mAuthProgressDialog.dismiss();
@@ -94,25 +88,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                         }
                     }
                 });
-
-    }
-    @Override
-    public void onClick(View view) {
-        if (view == mRegisterTextView) {
-            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
-            startActivity(intent);
-            finish();
-        }
-        if (view == mPasswordLoginButton) {
-             loginWithPassword();
-        }
     }
     @Override
     public void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
     }
-
     @Override
     public void onStop() {
         super.onStop();
@@ -121,5 +102,3 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 }
-
-
