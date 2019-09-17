68d2b78021c39773ae12b85561625c8320756399
Jeremic Arose <arosejeremic5@gmail.com>
2019-09-17 16:54:27 +0300

    add slide image animations

--- app/src/main/java/com/arose/myjokes/ui/MainActivity.java
+++ app/src/main/java/com/arose/myjokes/ui/MainActivity.java
@@ -12,9 +12,10 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 
-
+import com.arose.myjokes.ImageAdapter;
 import com.arose.myjokes.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -44,6 +45,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        ImageAdapter adapter = new ImageAdapter(this);
+        viewPager.setAdapter(adapter);
+
         ButterKnife.bind(this);
 
         mJokeButton.setOnClickListener(this);
